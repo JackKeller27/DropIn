95f7129cc8c0a2711214782f965c0a8b2e2e50b9
JackKeller27 <92999911+JackKeller27@users.noreply.github.com>
2022-10-22 02:18:41 -0400

    Style Login Screen, Create Map Screen

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { Button, Text, View, StyleSheet, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+//Home Screen
+
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.view}>
@@ -10,6 +12,7 @@ function HomeScreen({ navigation }) {
 
       <Button
         title="Login"
+        color="white"
         onPress={() => navigation.navigate('Login')}
       />
 
@@ -17,45 +20,66 @@ function HomeScreen({ navigation }) {
   );
 }
 
-const LoginScreen = ({ navigation }) => {
-  const [text, onChangeText] = React.useState(null);
+//Login Screen
+
+function LoginScreen({ navigation }) {
+  const [username, onChangeTextUsr] = React.useState(null);
+  const [password, onChangeTextPsw] = React.useState(null);
 
   return (
     <View
-      style={styles.view}>
+      style={styles.login}>
 
       <Text style={styles.titleText}>Login</Text>
 
       <TextInput
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={onChangeTextUsr}
+        value={username}
         placeholder="Username"
       />
 
       <TextInput
         style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        onChangeText={onChangeTextPsw}
+        value={password}
         placeholder="Password"
         secureTextEntry={true}
       />
+
+      <Button
+        title="Temp"
+        color="white"
+        onPress={() => navigation.navigate('Map')}
+      />
     </View>
-  )
+  );
 }
 
-const Stack = createNativeStackNavigator();
+//Map Screen
 
+function MapScreen({ navigation }) {
+  return (
+    <View></View>
+  );
+}
+
+//Navigation
+
+const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+//Style Sheet
 
 const styles = StyleSheet.create({
   view: {
@@ -65,20 +89,26 @@ const styles = StyleSheet.create({
     backgroundColor: "gold",
   },
 
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  login: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "gold",
   },
 
-  button: {
-
+  input: {
+    height: 40,
+    margin: 6,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 15,
+    backgroundColor: "white"
   },
 
   titleText: {
-    fontSize: 20,
-    fontWeight: "bold"
+    fontSize: 25,
+    fontWeight: "bold",
+    marginTop: 25
   },
 
   baseText: {
