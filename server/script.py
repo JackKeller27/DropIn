6b9fddbe855f47040a1134fd74b5cee226bbdf66
Dhruv Shah <90000344+DhruvShah09@users.noreply.github.com>
2022-10-23 04:03:09 -0400

    Test

--- server/script.py
+++ server/script.py
@@ -5,5 +5,5 @@ file2 = open('requirements.txt', 'r')
 Lines2 = file2.readlines()
 
 for line in Lines2:
-    file1.write(line.split('@')[0])
+    file1.write(line.split('=')[0])
     file1.write('\n')
