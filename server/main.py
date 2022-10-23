bba335380b311bd4a998e4d29b4e2753e676b081
Dhruv Shah <90000344+DhruvShah09@users.noreply.github.com>
2022-10-23 02:30:20 -0400

    Dhruv Commit

---
+++ server/main.py
@@ -0,0 +1,65 @@
+from flask import Flask, request, jsonify
+import objects as obj
+from pymongo import MongoClient
+import os
+
+LOCAL_SECRET  = os.getenv('LOCAL_SECRET')
+client = MongoClient('mongodb+srv://admin:admin@skatecluster.r8rwk1y.mongodb.net/?retryWrites=true&w=majority')
+db = client["skate"]
+collection = db["skate"]
+geo_collection = db["geodata"]
+
+app = Flask(__name__)
+app.secret_key = LOCAL_SECRET
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    requestContents = {}
+    requestContents = request.get_json()
+    if requestContents:
+        userObj = obj.User("", "")
+        document = userObj.parse_json_to_user(requestContents)
+        collection.insert_one(document)
+    else:
+        print("error parsing json")
+    return "User added"
+@app.route('/login', methods=['POST'])
+def login():
+    requestContents = {}
+    requestContents = request.get_json()
+    if requestContents:
+        userObj = obj.User("", "")
+        document = userObj.parse_json_to_user(requestContents)
+        query_login = userObj.create_login_query()
+        result = collection.find_one(query_login)
+        if result:
+            return "True"
+        else:
+            return "False"
+    else:
+        print("error parsing json")
+    return " "
+@app.route('/pushpin', methods=['POST'])
+def pushpin():
+    requestContents = {}
+    requestContents = request.get_json()
+    if requestContents:
+        locationObj = obj.PinLocation("", "", "")   
+        document = locationObj.parse_json_to_location(requestContents)
+        geo_collection.insert_one(document)
+        query = {"Username": requestContents["Username"]}
+        collection.update_one(query, {$push: {"Pins": locationObj.parse_json_to_location(requestContents)})
+    else:
+        print("error parsing json")
+        return "error"
+    return "Location added" 
+@app.route('/getpins', methods=['GET'])
+def getpins(): 
+    responseContents = {} 
+    responseContents['Pins'] = []
+    cursor = collection.find({})
+    for item in cursor:
+        responseContents['Pins'].append(item)
+    return jsonify(responseContents)
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
---
+++ server/objects.py
@@ -0,0 +1,57 @@
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.pins = []
+    def set_username(self, username):
+        self.username  = username
+    def set_password(self, password):
+        self.password = password
+    def parse_json_to_user(self, jsonDoc):
+        for key in jsonDoc:
+            if key == "Username":
+                self.username = jsonDoc[key]
+            if key == "Password":
+                self.password = jsonDoc[key]
+        return self.create_document_entry()
+    def create_document_entry(self):
+        userStruct = {} 
+        userStruct['Username'] = self.username
+        userStruct['Password'] = self.password    
+        userStruct['Pins'] = self.pins
+        return userStruct
+    def create_login_query(self):
+        query = {"Username": self.username, "Password": self.password}
+        return query 
+    def create_location_query(self):
+        query = {"Username": self.username}
+        return query 
+
+class PinLocation: 
+    def __init__(self, latitude, longitude, user):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.user = user
+    def set_user(self, user):
+        self.user = user
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+    def set_longitude(self, longitude):
+        self.longitude = longitude
+    def get_user(self):
+        return self.user
+    def parse_json_to_location(self, jsonDoc):
+        for key in jsonDoc:
+            if key == "Latitude":
+                self.latitude = jsonDoc[key]
+            if key == "Latitude":
+                self.latitude = jsonDoc[key]
+            if key == "User": 
+                self.user = jsonDoc[key]
+        return self.create_document_entry()
+    def create_document_entry(self): 
+        document = {}
+        document['Location'] = {"Latitude" : self.latitude, "Longitude" : self.longitude}
+        document['User_Create'] = self.user
+
+    
